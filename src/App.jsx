dad3603779969cbe35528c441526112a29daca27
Andres Ruiz Flores <andresrf74@gmail.com>
2024-12-02 14:52:13 -0300

    Creando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
 
-
-function App() {
+const App = () => {
   return (
+    <>
+      <Navbar />
       <div className="App">
-          <Navbar/>
-          <Footer/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/dentist/:id" element={<Detail />} />
+          <Route path="/favs" element={<Favs />} />
+        </Routes>
       </div>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
