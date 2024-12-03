cf2baec7856dbe4b0f4a580667a89c261ab481c5
Andres Ruiz Flores <andresrf74@gmail.com>
2024-12-03 12:19:30 -0300

    Mapeo de API en Home y creación de Card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+
+import { Link } from "react-router-dom";
 
 
 const Card = ({ name, username, id }) => {
 
   const addFav = ()=>{
     // Aqui iria la logica para agregar la Card en el localStorage
-  }
+    const currentFavs = JSON.parse(localStorage.getItem("favs")) || [];
+    
+    const isAlreadyFav = currentFavs.some(fav => fav.id === id);
+    if (isAlreadyFav) {
+      const updatedFavs = [...currentFavs, { name, username, id }];
+      localStorage.setItem("favs", JSON.stringify(updatedFavs));
+      alert(`${name} has been added to your favorites`);  
+    } else {
+      alert(`${name} is already in your favorites`);
+    }
+  };
 
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
-
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+        <img src="../../public/images/doctor.jpg" alt="doctor" />
+        <Link to={`/dentist/${id}`}><h4>{name}</h4></Link>
+        <p>{username}</p>
+        <h5>{id}</h5>
         <button onClick={addFav} className="favButton">Add fav</button>
     </div>
   );
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+
+import { useEffect, useState } from 'react';
 import Card from '../Components/Card'
+import axios from 'axios';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  const [dentists, setDentists] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        setDentists(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError("We were unable to load the data. Please try again later.", );
+      });
+  }, []);
   return (
     <main className="" >
       <h1>Home</h1>
       <div className='card-grid'>
         {/* Aqui deberias renderizar las cards */}
+        {error && <p>{error}</p>}
+        {dentists.map((dentist) => (
+          <Card key={dentist.id} name={dentist.name} username={dentist.username} id={dentist.id} />
+        ))}
       </div>
     </main>
   )
